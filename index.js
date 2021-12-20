06fd82779049e2fc5a1beb6a437042015fac1632
zhangHaha <zhaha_1122@163.coom>
2021-12-20 22:45:10 +0800

    '项目入口修该'

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ import type from './type/index.js'
 import reg from './reg/index.js'
 import env from './env/index.js'
 import dom from './dom/index.js'
+import bom from  './bom/index'
 import DataProcess from  './Data Processing/index'
+import utils from  './utils/index'
+import str from  './str/index'
+import num from  './num/index'
+import file from  './file/index'
+import func from  './func/index'
 export {
   // 类型检查
   type,
   reg,
   env,
   dom,
-  DataProcess
+  bom,
+  DataProcess,
+  utils,
+  str,
+  func
 }
